e6e5e72f59a51367250225bba18ddfa7e2d8f03e
wcesaret <wcesaret@iu.edu>
2024-04-22 20:01:50 -0400

    cmon

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -69,10 +69,10 @@ export const registrationUser = CatchAsyncError(async (req, res, next) => {
 
     // Set the activation token as an HTTP-only cookie
     res.cookie("activationToken", activationToken.token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // set to true if using https and in production
+      httpOnly: false,
+      secure: process.env.NODE_ENV === "development", // set to true if using https and in production
       maxAge: 300000, // 5 minutes in milliseconds
-      sameSite: "strict", // adjust according to your requirements
+      sameSite: "None", // adjust according to your requirements
     });
 
     const activationCode = activationToken.activationCode;
