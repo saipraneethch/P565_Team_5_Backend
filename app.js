4d3420e64cb58ff37663ef50c00eb894bb1f79d6
wcesaret <wcesaret@iu.edu>
2024-04-23 12:26:59 -0400

    added some headers stuff

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
